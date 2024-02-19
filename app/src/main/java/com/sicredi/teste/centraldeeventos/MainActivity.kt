f624dec306ab2e21a4a777e133f05e3840136e35
Andrew Patrick <mainframeutrono@gmail.com>
2024-02-19 16:07:35 -0300

    Initial commit

---
+++ app/src/main/java/com/sicredi/teste/centraldeeventos/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sicredi.teste.centraldeeventos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
